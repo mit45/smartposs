415944de310b0c56e7cb1b26c1caf4354440a845
Ümit Topuz <umittopuzg@gmail.com>
2025-10-19 20:01:55 +0300

    1. gün alıştırmalar

---
+++ practice.py
@@ -0,0 +1,9 @@
+import math_utils
+
+a = float(input("Birinci sayıyı girin: "))
+b = float(input("İkinci sayıyı girin: "))
+
+print(math_utils.topla(a, b))
+print(math_utils.cikar(a, b))
+print(math_utils.carp(a, b))
+print(math_utils.bol(a, b))
